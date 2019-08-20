e19f8cb5896bc0f9372b7049554d52daeb6882a7
Esmail <esmail.shabayek@gmail.com>
2019-08-20 11:12:39 +0200

    Load Customer Controller from modules

---
+++ modules/customer/Http/Controllers/CustomerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Customer\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Customer controller
+ */
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        dd('Customers Controller!!');
+    }
+
+}
